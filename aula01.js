a0c9ec5b5655228db3bf670847736f13dcd22666
João Vitor Santana <joao.sant.dev@gmail.com>
2024-10-16 20:18:20 -0300

    first commit

---
+++ aula01.js
@@ -0,0 +1,21 @@
+
+  
+
+  //------------------------------------------------------------------//
+
+  let resultado;
+
+  function somar(num1, num2){
+    resultado = num1 + num2
+  }
+
+  function mostrar(mensagem){
+    alert(mensagem)
+  }
+
+  let numero1 = parseInt(prompt ("Digite um numero")
+  
+
+  somar(numero1, numero2)
+
+  mostrar(resultado)
