3e69c8251cb5e127040283daebaebc3a14b80d7f
João Vitor Santana <joao.sant.dev@gmail.com>
2024-10-16 21:51:25 -0300

    first commit

--- aula01.js
+++ aula01.js
@@ -3,3 +3,11 @@ let anoAtual = 2024;
 let idade = anoAtual - digitadoPeloUsuario; 
 
 document.getElementById("resultado").innerText = "Sua idade é: " + idade;
+
+//if (idade > 80) {
+//  document.getElementById("resultado").innerText = "Não me leva a mal, MAS TUDO ISSO?";
+//}if else (idade < 0){
+//  document.getElementById("resultado").innerText = "Temos aqui um novo BAIJAMIN BUTTON: " + idade + "anos de idade é para poucos";
+//}else{
+//  document.getElementById("resultado").innerText - "Sua idade é"
+//}
